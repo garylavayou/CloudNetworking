0b4a58dbb5f2525fbd556890bc33d7ef3884addd
Gang Wang <gary.wang.uestc@outlook.com>
2016-12-26 19:45:34 +0800

    Version: 1

---
+++ NodeStaticCostOption.m
@@ -0,0 +1,14 @@
+classdef NodeStaticCostOption < uint32
+    %UNTITLED Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    enumeration
+        Random(1);
+        NetworkSpecified(2);
+    end
+    
+    methods
+    end
+    
+end
+
