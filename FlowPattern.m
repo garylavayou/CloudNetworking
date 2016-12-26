eb5c2820ba5478f2bf00f3cfd03a530d3853d169
Gang Wang <gary.wang.uestc@outlook.com>
2016-12-26 19:37:19 +0800

    Version: 1

---
+++ FlowPattern.m
@@ -0,0 +1,22 @@
+classdef FlowPattern < uint32
+    
+    properties
+    end
+    
+    methods
+    end
+    
+    enumeration
+        RandomSingleFlow(1);
+        RandomMultiFlow(2);
+        SingleSourceMultiFlow(3);
+        MultiSourceMultiFlow(4);
+    end
+end
+
+%% Flow Pattern
+% * *RandomSingleFlow*
+% * *RandomMultiFlow*
+% * *SingleSourceMultiFlow*
+% * *MultiSourceMultiFlow*
+
