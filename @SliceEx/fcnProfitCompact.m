4c4c814ef8bb5ec127dd58a2ba9a63bdf7a1a09c
Gang Wang <gary.wang.uestc@outlook.com>
2017-09-14 13:04:18 +0800

    New: Evaluate the objective function and gradient

---
+++ @SliceEx/fcnProfitCompact.m
@@ -0,0 +1,18 @@
+%% Evalute the objective function and gradient
+% only active independent variables are passed into the objective function.
+% Considering the constriant's coefficient matrix, if the corresponding column of the
+% coeffcient matrix for a variable is all zero, then this variable is inactive and can be
+% directly set as 0. So we can remove it from the optimization problem.
+%
+% NOTE: we can also remove the all-zero rows of the coefficient matrix, which do not
+% influence the number of variables. See also <optimalFlowRate>.
+function [profit, grad]= fcnProfitCompact(act_var_x, S)
+var_x = zeros(S.num_vars,1);
+var_x(S.I_active_variable) = act_var_x;
+
+% we extend the active variables by adding zeros to the inactive ones.
+[profit, grad] = Slice.fcnProfit(var_x, S);
+
+% eleminate the inactive variable's derivatives.
+grad = grad(S.I_active_variable);
+end
