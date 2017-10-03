60f265997340a3645c170afa0f8564d7cc09f0e3
Gang Wang <gary.wang.uestc@outlook.com>
2017-10-03 12:27:40 +0800

    Update variable names.

--- @PhysicalNetwork/AddSlice.m
+++ @PhysicalNetwork/AddSlice.m
@@ -69,43 +69,42 @@ end
 
 %% create flow table
 graph = this.residualgraph(slice_opt);
-[slice_opt.flow_table, phy_adjacent, flag] = this.generateFlowTable(graph, slice_opt);
+[slice_opt.FlowTable, phy_adjacent, flag] = this.generateFlowTable(graph, slice_opt);
 sl = [];
 if flag == false
     return;
 end
-number_flow = height(slice_opt.flow_table);
+number_flow = height(slice_opt.FlowTable);
 if number_flow == 0
     cprintf('comments', 'Info: all flow are rejected.\n');
     return;
 end
-slice_opt.flow_table.Properties.VariableNames = ...
+slice_opt.FlowTable.Properties.VariableNames = ...
     {'Source', 'Target', 'Rate', 'Delay', 'Paths'};
 
 phy_node_id = find(transpose(sum(phy_adjacent,1)~=0) | sum(phy_adjacent,2)~=0);
-slice_opt.adjacent = phy_adjacent(phy_node_id, phy_node_id);
-slice_opt.num_nodes = length(slice_opt.adjacent);
-slice_opt.num_edges = nnz(slice_opt.adjacent);
-slice_opt.node_map_S2P = phy_node_id;
+slice_opt.Adjacent = phy_adjacent(phy_node_id, phy_node_id);
+slice_opt.NumberNodes = length(slice_opt.Adjacent);
+slice_opt.NumberEdges = nnz(slice_opt.Adjacent);
+slice_opt.NodeMapS2P = phy_node_id;
 % slice_opt.node_price = ones(length(phy_node_id),1);
-slice_opt.node_map_P2S = zeros(this.NumberNodes,1);
-for i = 1:slice_opt.num_nodes
-    slice_opt.node_map_P2S(slice_opt.node_map_S2P(i)) = i;
+slice_opt.NodeMapP2S = zeros(this.NumberNodes,1);
+for i = 1:slice_opt.NumberNodes
+    slice_opt.NodeMapP2S(slice_opt.NodeMapS2P(i)) = i;
 end
 
-slice_opt.link_map_S2P = zeros(slice_opt.num_edges,1);
-slice_opt.link_map_P2S = zeros(this.NumberLinks,1);
-[head, tail] = find(slice_opt.adjacent~=0);
+slice_opt.LinkMapS2P = zeros(slice_opt.NumberEdges,1);
+slice_opt.LinkMapP2S = zeros(this.NumberLinks,1);
+[head, tail] = find(slice_opt.Adjacent~=0);
 %%
 % head and tail is the index of node in slice, this.graph.IndexEdge require the physical
 % ID of the node
-slice_opt.link_map_S2P = this.graph.IndexEdge(phy_node_id(head),phy_node_id(tail));
+slice_opt.LinkMapS2P = this.graph.IndexEdge(phy_node_id(head),phy_node_id(tail));
 % slice_opt.link_price = ones(length(head),1);
-slice_opt.link_map_P2S(slice_opt.link_map_S2P) = 1:length(head);
-slice_opt.flow_table.Source = slice_opt.node_map_P2S(slice_opt.flow_table.Source);
-slice_opt.flow_table.Target = slice_opt.node_map_P2S(slice_opt.flow_table.Target);
-slice_opt.parent = this;
-slice_opt = rmfield(slice_opt, 'method');
+slice_opt.LinkMapP2S(slice_opt.LinkMapS2P) = 1:length(head);
+slice_opt.FlowTable.Source = slice_opt.NodeMapP2S(slice_opt.FlowTable.Source);
+slice_opt.FlowTable.Target = slice_opt.NodeMapP2S(slice_opt.FlowTable.Target);
+slice_opt.Parent = this;
 
 % To perform the slice admitting control, we should first add the new slice into the
 % network, and perform resource allocation.
@@ -120,9 +119,8 @@ this.allocatepathid(sl);
 
 %% TODO
 % used for resource partitioning and pricing.
-slice_link_usage = slice_opt.link_map_P2S~=0;
+slice_link_usage = slice_opt.LinkMapP2S~=0;
 this.link_usage = [this.link_usage slice_link_usage];
-slice_node_usage = slice_opt.node_map_P2S~=0;
+slice_node_usage = slice_opt.NodeMapP2S~=0;
 this.node_usage = [this.node_usage slice_node_usage];
 end
-
