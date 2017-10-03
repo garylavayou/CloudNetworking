9dee1b54f8598d0a2d7475466cf6c3bd4e45bd87
Gang Wang <gary.wang.uestc@outlook.com>
2017-10-03 12:30:27 +0800

    call _updateDemandInfo_ and _generateEndPoints_ to generate flow information. update flow generation loop. Update the processing logic of options.

--- @PhysicalNetwork/generateFlowTable.m
+++ @PhysicalNetwork/generateFlowTable.m
@@ -1,57 +1,58 @@
 function [flow_table, phy_adjacent, flag] = generateFlowTable( this, graph, slice_opt )
 % generateFlowTable Generate random flows with random selected node pairs.
+% called by _AddSlice_ and _creatflow_.
 flow_table = table;
+if nargout >= 3
+    flag = true;
+end
 if nargout >= 2
     phy_adjacent = spalloc(this.NumberNodes, this.NumberNodes, this.NumberLinks);
 end
-switch slice_opt.FlowPattern
-    case FlowPattern.RandomSingleFlow
-        number_flow = 1;
-    case FlowPattern.RandomMultiFlow
-        number_flow = min(slice_opt.NumberFlows, this.NumberNodes*(this.NumberNodes-1));
-    otherwise
-        error('error: cannot handle the flow pattern <%s>.', ...
-                        slice_opt.FlowPattern.char);
+options = this.updatePathConstraints(slice_opt);
+info = this.updateDemandInfo(slice_opt);
+if isfield(slice_opt, 'DuplicateFlow') && slice_opt.DuplicateFlow==true
+    b_duplicate = true;
+    number_flow = slice_opt.NumberFlows;
+else
+    b_duplicate = false;
+    number_flow = min(slice_opt.NumberFlows, info.NumberFlows);
 end
 
 k = 0;
-options = this.updatePathConstraints(slice_opt);
-if isfield(slice_opt, 'NodeSet')
-    numnodes = length(slice_opt.NodeSet);
-    nodeset = slice_opt.NodeSet;
-else
-    numnodes = this.Topology.numnodes;
-    nodeset = 1:numnodes;
-end
-
-flag = true;
 while k < number_flow
-    end_points = nodeset(unique_randi(numnodes, 2, 'stable'));
-    if height(flow_table)==0 || isempty(find(flow_table{:,1}==end_points(1) & ...
-            flow_table{:,2}==end_points(2),1))
-        k = k+1;
-        path_list = graph.CandidatePaths(slice_opt.NumberPaths, ...
-            end_points(1), end_points(2), options);
-        % assert path list
-        switch this.assert_path_list(end_points, path_list, slice_opt)
-            case -1  % failed with error;
-                error('error: cannot find feasible path between %d and %d.',...
-                    end_points(1), end_points(2));
-            case 0  % succeed
-            case 1  % failed but continue
-                continue;
-            case 2  % failed and return;
-                flag = false;
-                return;
-        end
-        flow_table(end+1,:) = {end_points(1),end_points(2),0,0,path_list}; %#ok<AGROW>
-        if nargin >= 2
-            for p = 1:path_list.Width
-                path = path_list.paths{p};
-                for l = 1:(path.Length-1)
-                    phy_adjacent(path.Node(l), path.Node(l+1)) = ...
-                        graph.Adjacent(path.Node(l), path.Node(l+1));  %#ok<SPRIX>
-                end
+    %% TODO the source and targets can be the same node
+    % In this case, the flow only consumes computing resources on data centers.
+    end_points = this.generateEndPoints(info, slice_opt);
+    if height(flow_table)>0 && ~b_duplicate && ...
+            ~isempty(find(flow_table{:,1}==end_points(1) & flow_table{:,2}==end_points(2),1)) 
+        continue;
+    end
+    k = k+1;
+    path_list = graph.CandidatePaths(slice_opt.NumberPaths, ...
+        end_points(1), end_points(2), options);
+    % assert path list
+    switch this.assert_path_list(end_points, path_list, slice_opt)
+        case -1  % failed with error;
+            error('PhysicalNetwork:Disconnected', ...
+                'error: cannot find feasible path between %d and %d.', ...
+                end_points(1), end_points(2));
+        case 0  % succeed
+        case 1  % failed but continue
+            continue;
+        case 2  % failed and return;
+            flag = false;
+            return;
+    end
+    % Path list records the physical nodes on paths. Latterly, it will be transformed
+    % into virtual nodes. See also <AddSlice> and <createflow>.
+    flow_table(end+1,:) = ...
+        {end_points(1),end_points(2),0,path_list.Latency,path_list}; %#ok<AGROW>
+    if nargout >= 2
+        for p = 1:path_list.Width
+            path = path_list.paths{p};
+            for l = 1:(path.Length-1)
+                phy_adjacent(path.Node(l), path.Node(l+1)) = ...
+                    graph.Adjacent(path.Node(l), path.Node(l+1));  %#ok<SPRIX>
             end
         end
     end
