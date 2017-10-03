97219d909cfe926384acca8de54e7f4463f067f3
Gang Wang <gary.wang.uestc@outlook.com>
2017-10-03 12:27:15 +0800

    Update variable names.

--- @DynamicSlice/fcnSocialWelfare.m
+++ @DynamicSlice/fcnSocialWelfare.m
@@ -3,28 +3,27 @@
 % Slice resource cost is fixed, since the price is given and the resource amount is known.
 % When using the |'fixcost'| option, the slice's resource cost is
 % constant, and we do not consider it in the objective and thus in the gradient.
-function [profit, grad] = fcnSocialWelfare(var_x, S, model)
-if strcmp(model, 'fixcost')
+function [profit, grad] = fcnSocialWelfare(var_x, S, options)
+if strcmp(options.CostModel, 'fixcost')
     var_path = var_x(1:S.NumberPaths);
     flow_rate = S.getFlowRate(var_path);
-    weight = S.weight*ones(S.NumberFlows, 1);
-    profit = -sum(weight.*fcnUtility(flow_rate));
+    profit = -sum(S.weight*fcnUtility(flow_rate));
 
     if nargout <= 1
         profit = -profit;
     else
         grad = spalloc(length(var_x),1, S.NumberPaths);
         for p = 1:S.NumberPaths
-            i = S.path_owner(p);
-            f = S.I_flow_path(:,p)~=0;  % find the weight of the flow on the path.
-            grad(p) = -weight(f)/(1+S.I_flow_path(i,:)*var_path); %#ok<SPRIX>
+            i = S.path_owner(p);  % find the the owner (flow) of the path.
+                                  % f = S.I_flow_path(:,p)~=0;  
+            grad(p) = -S.weight/(1+S.I_flow_path(i,:)*var_path); %#ok<SPRIX>
         end
     end
 else
     if nargout <= 1
-        profit = fcnSocialWelfare@Slice(var_x, S, model);
+        profit = fcnSocialWelfare@Slice(var_x, S);
     else
-        [profit, grad] = fcnSocialWelfare(var_x, S, model);
+        [profit, grad] = fcnSocialWelfare(var_x, S);
     end
 end
 end
