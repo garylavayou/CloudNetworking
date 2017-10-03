8b8e5cc3de190c99d3920e091ba517d847d28641
Gang Wang <gary.wang.uestc@outlook.com>
2017-10-03 12:36:05 +0800

    Update variable names.

--- @Slice/loadSliceTemplate.m
+++ @Slice/loadSliceTemplate.m
@@ -11,7 +11,7 @@ for i = 1:length(type_index)
     switch type_index(i)
         case 11      % Type 1 for experiment 1
             slice_template(i).Type = 11;
-            slice_template(i).Pattern = FlowPattern.RandomMultiFlow;
+            slice_template(i).FlowPattern = FlowPattern.RandomMultiFlow;
             slice_template(i).Weight = 15;
             slice_template(i).NumberPaths = 1;
             slice_template(i).NumberFlows = 30;
@@ -23,7 +23,7 @@ for i = 1:length(type_index)
             % slice_opt.VNFList = [];
         case 12     % Type 2 for experiment 1
             slice_template(i).Type = 12;
-            slice_template(i).Pattern = FlowPattern.RandomMultiFlow;
+            slice_template(i).FlowPattern = FlowPattern.RandomMultiFlow;
             slice_template(i).Weight = 40;
             slice_template(i).NumberPaths = 2;
             slice_template(i).NumberFlows = 10;
@@ -33,7 +33,7 @@ for i = 1:length(type_index)
             slice_template(i).ConstantProfit = 100;
         case 13  % Type 3 for experiment 1
             slice_template(i).Type = 13;
-            slice_template(i).Pattern = FlowPattern.RandomMultiFlow;
+            slice_template(i).FlowPattern = FlowPattern.RandomMultiFlow;
             slice_template(i).Weight = 200;
             slice_template(i).NumberPaths = 3;
             slice_template(i).NumberFlows = 3;
@@ -43,7 +43,7 @@ for i = 1:length(type_index)
             slice_template(i).ConstantProfit = 100;
         case 21  % Type 1 for experiment 2
             slice_template(i).Type = 21;
-            slice_template(i).Pattern = FlowPattern.RandomMultiFlow;
+            slice_template(i).FlowPattern = FlowPattern.RandomMultiFlow;
             slice_template(i).Weight =25;
             slice_template(i).NumberPaths = 1;
             slice_template(i).NumberFlows = 120;    % at most 15*14 flows
@@ -54,7 +54,7 @@ for i = 1:length(type_index)
             slice_template(i).ConstantProfit = 600;
         case 22  % Type 2 for experiment 2
             slice_template(i).Type = 22;
-            slice_template(i).Pattern = FlowPattern.RandomMultiFlow;
+            slice_template(i).FlowPattern = FlowPattern.RandomMultiFlow;
             slice_template(i).Weight = 50;
             slice_template(i).NumberPaths = 2;
             slice_template(i).NumberFlows = 40;
@@ -65,7 +65,7 @@ for i = 1:length(type_index)
             slice_template(i).ConstantProfit = 600;
         case 23  % Type 3 for experiment 2
             slice_template(i).Type = 23;
-            slice_template(i).Pattern = FlowPattern.RandomMultiFlow;
+            slice_template(i).FlowPattern = FlowPattern.RandomMultiFlow;
             slice_template(i).Weight = 300;
             slice_template(i).NumberPaths = 3;
             slice_template(i).NumberFlows = 4;
@@ -76,7 +76,7 @@ for i = 1:length(type_index)
             slice_template(i).ConstantProfit = 600;
         case 31     % Type 1 for experiment 3
             slice_template(i).Type = 31;
-            slice_template(i).Pattern = FlowPattern.RandomInterDataCenter;
+            slice_template(i).FlowPattern = FlowPattern.RandomInterDataCenter;
             slice_template(i).Weight = 40;
             slice_template(i).NumberPaths = 1;
             slice_template(i).NumberFlows = 25;
@@ -85,7 +85,7 @@ for i = 1:length(type_index)
             slice_template(i).Probability = [];
         case 32      % Type 2 for experiment 3
             slice_template(i).Type = 32;
-            slice_template(i).Pattern = FlowPattern.RandomInterBaseStation;
+            slice_template(i).FlowPattern = FlowPattern.RandomInterBaseStation;
             slice_template(i).Weight = 10;
             slice_template(i).NumberPaths = 2;
             slice_template(i).NumberFlows = 100;
@@ -94,7 +94,7 @@ for i = 1:length(type_index)
             slice_template(i).Probability = 1/6;
         case 33      % Type 3 for experiment 3
             slice_template(i).Type = 33;
-            slice_template(i).Pattern = FlowPattern.RandomDataCenter2BaseStation;
+            slice_template(i).FlowPattern = FlowPattern.RandomDataCenter2BaseStation;
             slice_template(i).Weight = 100;
             slice_template(i).NumberPaths = 3;
             slice_template(i).NumberFlows = 20;
@@ -114,10 +114,10 @@ for i = 1:length(type_index)
             % specify the average number of flows of this slice, add the initial number of
             % flows in the slice. |ArrivalRate| and |ServiceInterval| specifiy the flow's
             % dynamic attributes.
-            slice_template(i).NumberFlows = 60;         % 25/45
+            slice_template(i).NumberFlows = 40;         % 25/45
             slice_template(i).Flow.ArrivalRate = 15; % 25 arrivals/hour  
             slice_template(i).Flow.ServiceInterval = 1; 
-            slice_template(i).Pattern = FlowPattern.RandomInterDataCenter;
+            slice_template(i).FlowPattern = FlowPattern.RandomInterDataCenter;
         case 42     % Type 2 for experiment 4
             slice_template(i).Type = 42;
             slice_template(i).Weight = 10;
@@ -128,7 +128,7 @@ for i = 1:length(type_index)
             slice_template(i).NumberFlows = 100;
             slice_template(i).Flow.ArrivalRate = 1000; % 1000 arrivals/hour  
             slice_template(i).Flow.ServiceInterval = 0.1; 
-            slice_template(i).Pattern = FlowPattern.RandomInterBaseStation;
+            slice_template(i).FlowPattern = FlowPattern.RandomInterBaseStation;
         case 43     % Type 3 for experiment 4
             slice_template(i).Type = 43;
             slice_template(i).Weight = 100;
@@ -139,19 +139,19 @@ for i = 1:length(type_index)
             slice_template(i).NumberFlows = 20;
             slice_template(i).Flow.ArrivalRate = 100; % 100 arrivals/hour  
             slice_template(i).Flow.ServiceInterval = 0.2; 
-            slice_template(i).Pattern = FlowPattern.RandomDataCenter2BaseStation;
-        case 44     % Type 1 for experiment 4-2: EventDrivenNetork
+            slice_template(i).FlowPattern = FlowPattern.RandomDataCenter2BaseStation;
+        case 44     % Type 1 for experiment 4-2: DynamicNetwork
             slice_template(i).Type = 44;
             slice_template(i).Weight = 25;
             slice_template(i).NumberPaths = 1;
             slice_template(i).VNFList = [1,2,3];
-            slice_template(i).ArrivalRate = 0.05;   % 0.05 arrivals/hour  =>  20 hours/arrival
-            slice_template(i).ServiceInterval = 100;
-            slice_template(i).NumberFlows = 60;
-            slice_template(i).Flow.ArrivalRate = 30; % 25 arrivals/hour  
+            slice_template(i).ArrivalRate = 0.01;   % 0.01 arrivals/min  =>  100 min/arrival
+            slice_template(i).ServiceInterval = 300;
+            slice_template(i).NumberFlows = 80;
+            slice_template(i).Flow.ArrivalRate = 120; % 1/120 min/arrival
             slice_template(i).Flow.ServiceInterval = 0.5; 
-            slice_template(i).Pattern = FlowPattern.RandomMultiFlow;
-        case 45     % Type 2 for experiment 4-2: EventDrivenNetork
+            slice_template(i).FlowPattern = FlowPattern.RandomMultiFlow;
+        case 45     % Type 2 for experiment 4-2: DynamicNetwork
             slice_template(i).Type = 45;
             slice_template(i).Weight = 40;
             slice_template(i).NumberPaths = 2;
@@ -161,8 +161,8 @@ for i = 1:length(type_index)
             slice_template(i).NumberFlows = 20;
             slice_template(i).Flow.ArrivalRate = 1000; % 1000 arrivals/hour  
             slice_template(i).Flow.ServiceInterval = 0.1; 
-            slice_template(i).Pattern = FlowPattern.RandomMultiFlow;
-        case 46     % Type 3 for experiment 4-2: EventDrivenNetork
+            slice_template(i).FlowPattern = FlowPattern.RandomMultiFlow;
+        case 46     % Type 3 for experiment 4-2: DynamicNetwork
             slice_template(i).Type = 46;
             slice_template(i).Weight = 200;
             slice_template(i).NumberPaths = 3;
@@ -170,10 +170,9 @@ for i = 1:length(type_index)
             slice_template(i).ArrivalRate = 4;   % 4 arrivals/hour
             slice_template(i).ServiceInterval = 4;
             slice_template(i).NumberFlows = 5;
-            slice_template(i).NumberVNFs = 4;
             slice_template(i).Flow.ArrivalRate = 100; % 100 arrivals/hour  
             slice_template(i).Flow.ServiceInterval = 0.2; 
-            slice_template(i).Pattern = FlowPattern.RandomMultiFlow;
+            slice_template(i).FlowPattern = FlowPattern.RandomMultiFlow;
         otherwise
             error('error: unrecognized slice type.')
     end
