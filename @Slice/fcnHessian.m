17d93fd6bdb7e7ca3e7e370a2d7f1541b61fc3f1
Gang Wang <gary.wang.uestc@outlook.com>
2017-11-02 11:42:59 +0800

    Simplify arguments check.

--- @Slice/fcnHessian.m
+++ @Slice/fcnHessian.m
@@ -1,5 +1,7 @@
 %% Hessian matrix of the Lagrangian
 % Called by _singleSliceOptimization_;
+% To accelarate executing the function, no thorough arguments checking will be done. 
+% Throw errors, if necessary fields are not provided.
 %%
 % Since the problem only contains linear constraint, the Hessian matrix of the
 % Lagrangian is equal to the second-derivatives of the objective function, and the
@@ -11,34 +13,29 @@
 % $$\frac{\partial f}{\partial x(p)^2} =
 %    \frac{w\cdot q_{i_p,p_0}}
 %    {(1+\sum_{p_0\in\mathcal{P}}{q_{i_p,p_0}\cdot x_{p_0}})^2}$$
-function hess = fcnHessian(vars, lambda, S, options) %#ok<INUSL>
-if isempty(S.weight)
-    weight = S.FlowTable.Weight;    % for single slice;
+%% Method declaration
+%   hess = fcnHessian(vars, lambda, slice, options)
+% # Input Arguments
+% * *options*
+%       - |PricingPolicy|:
+function hess = fcnHessian(vars, lambda, slice, options) %#ok<INUSL>
+if isempty(slice.weight)
+    weight = slice.FlowTable.Weight;    % for single slice;
 else
-    weight = S.weight*ones(S.NumberFlows, 1);  % for multiple slices
+    weight = slice.weight*ones(slice.NumberFlows, 1);  % for multiple slices
 end
-NP = S.NumberPaths;
+NP = slice.NumberPaths;
 hess = spalloc(length(vars),length(vars), NP^2);
 var_path = vars(1:NP);
 for p = 1:NP
-    i = S.path_owner(p);
+    i = slice.path_owner(p);
     hess(p,1:NP) = weight(i)*...
-        S.I_flow_path(i,:)/(1+(S.I_flow_path(i,:)*var_path))^2; %#ok<SPRIX>
+        slice.I_flow_path(i,:)/(1+(slice.I_flow_path(i,:)*var_path))^2; %#ok<SPRIX>
 end
 
-if nargin==4
+if nargin >= 4
     if ~isfield(options, 'PricingPolicy')
         return;
-    end
-    if isfield(options, 'LinkPrice')
-        link_price = options.LinkPrice;
-    else
-        link_price = S.VirtualLinks.Price;
-    end
-    if isfield(options, 'NodePrice')
-        node_price = options.NodePrice;
-    else
-        node_price = S.VirtualDataCenters.Price;
     end
     switch options.PricingPolicy
         case 'quadratic-price'
@@ -55,12 +52,12 @@ if nargin==4
             % Then, for all paths, the hessian matrix component can represented by 
             % $G^T D_e\c G$, where matrix $G(e,p) = g_{e,p}$, $D_e$ is the
             % diagnoal matrix for $\rho_e^{''}$.
-            [~,~,lph] = S.fcnLinkPricing(link_price, S.getLinkLoad(var_path));
-            h1 = (S.I_edge_path') * lph * S.I_edge_path;
+            [~,~,lph] = slice.fcnLinkPricing(slice.prices.Link, slice.getLinkLoad(var_path));
+            h1 = (slice.I_edge_path') * lph * slice.I_edge_path;
             hess(1:NP, 1:NP) = hess(1:NP, 1:NP) + h1;
-            var_node = vars((NP+1):end);
-            [~,~,nph] = S.fcnNodePricing(node_price, S.getNodeLoad(var_node));
-            NC = S.NumberDataCenters;
+            var_node = vars((NP+1):slice.num_vars);
+            [~,~,nph] = slice.fcnNodePricing(slice.prices.Node, slice.getNodeLoad(var_node));
+            NC = slice.NumberDataCenters;
             %%%
             % Second derivatives on the node payment componet:
             % 
@@ -89,14 +86,14 @@ if nargin==4
                     % we only calculate the lower triangle, and use the symetric property
                     % to fill the upper triangle.
                     h2(z_index1,z_index2) = ...
-                        diag(S.I_node_path(:,p1) .* nph .* S.I_node_path(:,p2));
+                        diag(slice.I_node_path(:,p1) .* nph .* slice.I_node_path(:,p2)); %#ok<SPRIX>
                     z_index2 = z_index2 + NC;
                 end
                 z_index1 = z_index1 + NC;
             end
             h2 = h2 + (tril(h2,-1))';   % fill the upper triangle since the
-            h2 = repmat(h2, S.NumberVNFs, S.NumberVNFs);
-            hess((NP+1):end, (NP+1):end) = h2;
+            h2 = repmat(h2, slice.NumberVNFs, slice.NumberVNFs);
+            hess((NP+1):slice.num_vars, (NP+1):slice.num_vars) = h2;
         otherwise
     end
 end
