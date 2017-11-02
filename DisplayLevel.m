f512b1d528f7fabb03888e149cf6043fed6b7b7e
Gang Wang <gary.wang.uestc@outlook.com>
2017-11-02 12:54:29 +0800

    New.

---
+++ DisplayLevel.m
@@ -0,0 +1,14 @@
+classdef DisplayLevel < uint32    
+    enumeration
+        Off(0),
+        Final(1),
+        Notify(2),
+        Iteration(3),
+        Iter(4)
+    end
+    
+    methods
+    end
+    
+end
+
---
+++ FlowType.m
@@ -0,0 +1,13 @@
+classdef FlowType < uint32
+    
+    enumeration
+        Normal(0),
+        Adhoc(1),
+        Handover(2)
+    end
+    
+    methods
+    end
+    
+end
+
