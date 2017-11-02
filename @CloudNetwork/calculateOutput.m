683fd9ccd18727f9c2f6fbd525fb51b8017f86f0
Gang Wang <gary.wang.uestc@outlook.com>
2017-11-02 11:19:19 +0800

    Add option handling; Add partial slices support.

--- @CloudNetwork/calculateOutput.m
+++ @CloudNetwork/calculateOutput.m
@@ -7,11 +7,22 @@
 % # Flow rate of all flows in the network.
 %%
 % |argin|: existing field for output.
-function argout = calculateOutput(this, argin, new_opts)
-if ~isempty(argin)
+function argout = calculateOutput(this, argin, options)
+if nargin>= 2 && ~isempty(argin)
     argout = argin;
 end
-    
+if nargin < 3
+    options = struct;
+end
+options = getstructfields(options, {'Slices', 'PricingPolicy'}, 'default-ignore', ...
+    {this.slices,[]});
+if isempty(options.PricingPolicy)
+    options.PricingPolicy = 'linear';
+    warning('Slice.calculateOutput: <PricingPolicy> set to default [%s].', ...
+        options.PricingPolicy);
+end
+options.bFinal = true;
+
 argout.LinkPrice = this.getLinkField('Price');
 argout.NodePrice = this.getNodeField('Price');            
 argout.LinkLoad = this.getLinkField('Load');
@@ -21,18 +32,15 @@ argout.Welfare = 0;
 
 profit_table = zeros(this.NumberSlices+1, 1);
 
-for s = 1:this.NumberSlices
-    sl = this.slices{s};
+for s = 1:length(options.Slices)
+    sl = options.Slices{s};
     argout.FlowRate = [argout.FlowRate; sl.FlowTable.Rate];
-    var_x = [sl.Variables.x; sl.Variables.z];
-    
     %% Calculate the net social welfare
     %       The net social welfare is the total utility less the total network cost.
     argout.Welfare = argout.Welfare + sl.weight*sum(fcnUtility(sl.FlowTable.Rate));
     
     % Calculate the profit of slices
-    profit_table(s) = Slice.fcnProfit(var_x, sl, ...
-        getstructfields(new_opts, {'PricingPolicy'}));
+    profit_table(s) = sl.getProfit(options);
     %%%
     % * *Net profit with offered price*
     %
