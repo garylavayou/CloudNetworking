b2c8623d36c49393e39f87de0ac615b52c141316
Gang Wang <gary.wang.uestc@outlook.com>
2017-11-10 19:47:45 +0800

    Move
    methods move to @Slice.

--- @SliceEx/fcnHessianCompact.m
+++
@@ -1,7 +0,0 @@
-%% Compact form of Hessian matrix of the Largrangian
-function hess = fcnHessianCompact(act_var_x, ~, S)
-var_x = zeros(S.num_vars,1);
-var_x(S.I_active_variable) = act_var_x;
-hess = Slice.fcnHessian(var_x, [], S);
-hess = hess(S.I_active_variable, S.I_active_variable);
-end
