7523deb88e5703a23964bbb53f7860b5eff0895f
Gang Wang <gary.wang.uestc@outlook.com>
2017-11-10 19:49:16 +0800

    New
    Derived from @DynamicSlice, for test purpose.

---
+++ DynamicSliceTest.m
@@ -0,0 +1,31 @@
+classdef DynamicSliceTest < DynamicSlice
+%% 
+% Add a warm-up phase for experiment, in the warm-up phase we only use the 'dimconfig' method to
+% reconfigure slices. 
+    properties (Access = protected)
+        saved_options;
+    end
+    
+    methods
+        function this = DynamicSliceTest(slice_data)
+            this@DynamicSlice(slice_data);
+            this.saved_options = this.options;
+            this.options = structmerge(this.options, ...
+                getstructfields(slice_data, 'NumberEventWarmUp','default-ignore', 50));
+            this.options.Method = 'dimconfig';
+        end
+    end
+    
+    
+    methods(Access=protected)
+        function [exitflag,fidx] = executeMethod(this, action)
+            global event_num DEBUG;  %#ok<NUSED>
+            [exitflag,fidx] = executeMethod@DynamicSlice(this, action);
+            if event_num == this.getOption('NumberEventWarmUp')
+                this.options.Method = this.saved_options.Method;
+            end
+        end
+    end
+    
+end
+
