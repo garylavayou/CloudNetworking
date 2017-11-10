546cb3334b18f8bda9fbde9fc76ad88ff923b1a4
Gang Wang <gary.wang.uestc@outlook.com>
2017-11-10 20:22:43 +0800

    Update
    add netwotk options.

--- run_exp1.m
+++ run_exp1.m
@@ -4,17 +4,20 @@
 
 %% Specification of Substrate Network
 % The parameters are determined in <run_test1.m>.
-link_opt.delay = LinkDelayOption.Random;
-link_opt.cost = LinkCostOption.CapacityInverse;
+clearvars -except progress_bar;
+clear global;
+link_opt.DelayModel = LinkDelayOption.Random;
+link_opt.CostModel = LinkCostOption.CapacityInverse;
 link_opt.CostUnit = 150;
 link_opt.CapacityFactor = 30;
-net_opt.delta = 0.7;
+link_opt.RandomSeed = 20171013;
+% net_opt.Delta = 0.7;
 
-node_opt.model = NetworkModel.Sample1;
-node_opt.capacity = NodeCapacityOption.BandwidthProportion;
-node_opt.cost = NodeCostOption.CapacityInverse;
+node_opt.Model = NetworkModel.Sample1;
+node_opt.CapacityModel = NodeCapacityOption.BandwidthProportion;
+node_opt.CostModel = NodeCostOption.CapacityInverse;
 node_opt.CostUnit = 500;
-node_opt.capacity_factor = 1.5;     % [0.3; 0.5; 0.8; 1]
+node_opt.CapacityFactor = 1.5;     % [0.3; 0.5; 0.8; 1]
 
 %% Specification of VNFs and Network Slices
 % |StaticCostOption| is not set, the default value is |None|;
@@ -22,27 +25,31 @@ node_opt.capacity_factor = 1.5;     % [0.3; 0.5; 0.8; 1]
 % efficiency.
 VNF_opt.Number = 4;            % number of VNF type
 VNF_opt.Model = VNFIntegrateModel.AllInOne;
-VNF_opt.RandomSeed = [20161101 0];       
+VNF_opt.RandomSeed = [20161101 0];
+net_opt.PricingFactor = 1;
+net_opt.AdmitPolicy = 'reject-flow';
+net_opt.ClassName = 'CloudNetwork';
+net_opt.Form = 'compact';
+net_opt.Threshold = 'min';
 
-%% Arrival sequence confiuration
+%% Arrival sequence configuration
 % Assume that the arrival interval is fixed, *only adjust the service lifetime and the
 % arrival probability* of each type of slices to control the number of slices in the
 % network. See also <RequestEvent>.
-type.Index = [1; 2; 3];
-type.Fixed = 1;
+type.Index = [11; 21; 31];
+type.Fixed = 1;           % corresponding index in type.Index;
 type.FixedCount = 3;      % Number of persistent slices: {1|2|3...}
 % type.PartitionWeight = [1 2 4];
-seed = 20161231;        % -> seed_dynamic
+seed_dynamic = 20161231;
 arrival.Number = 100;
 arrival.Interval = 0.25;     % Average of arrival interval.
 
 %% Algorithm options
-options.PricingFactor = 1;
-options.ProfitType = {'ApproximatePrice','AccuratePrice'};
-options.WelfareType = {'Accurate', 'Approximate'};
+% options.ProfitType = {'ApproximatePrice','AccuratePrice'};        moveto ex
+% options.WelfareType = {'Accurate', 'Approximate'};
 
 %% Control Variables
-options.Display = 'off';
+declare_info_level('Global', DisplayLevel.Off);
 b_single_optimal = true;
 b_price_adjust = true;
 b_static_slice = true;
@@ -50,9 +57,8 @@ b_dual_decomp = false;
 b_price_adjust2 = false;
 b_resource_part = false;
 b_part_price = false;
-
+b_save = true;
 %%
-load('output_seq11.mat')
 run_sequence;
 %%
 x_tick = 1:160;
@@ -61,7 +67,11 @@ xlimits = [stat.times(1), stat.times(x_tick(end))];
 fig3_ylimits = [200, 700;
     0, 1000;
     500, 2500];
+slice_profit_limit = [0, 2500; 0, 2500; 0, 2500];
 data_plot;
 %%
-save('output_seq11.mat', 'profit_accurate', 'profit_approx', ...
-'profit_stat', 'rate_stat', 'runtime', 'stat', 'utilization', 'type');
+if b_save
+    description = 'experiment (1-1): threshold = min.';
+    save('Results\output_seq11.mat', 'stat', 'stat_optimal', 'stat_price', ...
+        'stat_static', 'description');
+end
--- run_exp2.m
+++ run_exp2.m
@@ -1,21 +1,24 @@
 %% Run a sequence of network slice
 % comparison of methods, global optimization, resource pricing, static partitioning, and
 % static slicing method.
-load('output_seq21.mat')
+% load('output_seq21.mat')
 
 %% Specification of Substrate Network
 % The parameters are determined in <run_test2.m>.
-link_opt.delay = LinkDelayOption.Random;
-link_opt.cost = LinkCostOption.CapacityInverse;
-link_opt.CapacityFactor = 1000;
+clearvars -except progress_bar;
+clear global;
+link_opt.DelayModel = LinkDelayOption.Random;
+link_opt.CostModel = LinkCostOption.CapacityInverse;
 link_opt.CostUnit = 300;
-net_opt.delta = 0.5;
+link_opt.CapacityFactor = 1000;
+link_opt.RandomSeed = 20171017;
+% net_opt.Delta = 0.5;
 
-node_opt.model = NetworkModel.Sample2;
-node_opt.capacity = NodeCapacityOption.BandwidthProportion;
-node_opt.cost = NodeCostOption.CapacityInverse;
+node_opt.Model = NetworkModel.Sample2;
+node_opt.CapacityModel = NodeCapacityOption.BandwidthProportion;
+node_opt.CostModel = NodeCostOption.CapacityInverse;
 node_opt.CostUnit = 500;
-node_opt.capacity_factor = 1.5;     % [0.3; 0.5; 0.8; 1]
+node_opt.CapacityFactor = 1.5;     % [0.3; 0.5; 0.8; 1]
 
 %% Specification of VNFs and Network Slices
 % |StaticCostOption| is not set, the default value is |None|;
@@ -23,27 +26,27 @@ node_opt.capacity_factor = 1.5;     % [0.3; 0.5; 0.8; 1]
 % efficiency.
 VNF_opt.Number = 6;            % number of VNF type
 VNF_opt.Model = VNFIntegrateModel.AllInOne;
-VNF_opt.RandomSeed = [20161101 0]; 
+VNF_opt.RandomSeed = [20161101 0];
+net_opt.ClassName = 'CloudNetwork';
+net_opt.AdmitPolicy = 'reject-flow';
+net_opt.PricingFactor = 1;
+net_opt.Form = 'compact';
+net_opt.Threshold = 'min';
 
 %% Arrival sequence configuration
 % Assume that the arrival interval is fixed, *only adjust the service lifetime and the
 % arrival probability* of each type of slices to control the number of slices in the
 % network. See also <RequestEvent>.
-type.Index = [4; 5; 6]; 
+type.Index = [12; 22; 32];
 type.Fixed = 1;
 type.FixedCount = 3;      % Number of persistent slices: {1|2|3...}
-% type.PartitionWeight = [1 2 4];
-seed = 20170415;   % 20161231   -> seed_dynamic
+% type.PartitionWeight = [1 2 4];  >> Move to Ex
+seed_dynamic = 20170415;   % 20161231
 arrival.Number = 100;
 arrival.Interval = 1/6;     % Average of arrival interval.  
 
-%% Algorithm options
-options.PricingFactor = 1;
-options.ProfitType = {'ApproximatePrice','AccuratePrice'};
-options.WelfareType = {'Accurate', 'Approximate'};
-
 %% Control Variables
-options.Display = 'off';
+declare_info_level('Global', DisplayLevel.Off);
 b_single_optimal = true;
 b_price_adjust = true;
 b_static_slice = true;
@@ -51,7 +54,7 @@ b_dual_decomp = false;
 b_price_adjust2 = false;
 b_resource_part = false;
 b_part_price = false;
-
+b_save = true;
 %%
 run_sequence;
 
@@ -62,7 +65,11 @@ xlimits = [stat.times(1), stat.times(x_tick(end))];
 fig3_ylimits = [1000, 8000;
     1000, 8000;
     1000, 8000];
+slice_profit_limit = [0, 7000; 0, 7000; 0, 7000];
 data_plot;
 %%
-save('output_seq21.mat', 'profit_accurate', 'profit_approx', ...
-    'profit_stat', 'rate_stat', 'runtime', 'stat', 'utilization', 'type');
+if b_save
+    description = 'experiment (2-1): topology = Sample2, threshold = min.';
+    save('Results\output_seq21.mat', 'stat', 'stat_optimal', 'stat_price', 'stat_static',...
+        'description');
+end
