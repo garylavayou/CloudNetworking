8bfeec2fce3f2b25841c529b981899bdac4ee2ee
Gang Wang <gary.wang.uestc@outlook.com>
2017-11-10 20:12:35 +0800

    Modify
    remove arguments check.

--- @SliceEx/fcnProfit.m
+++ @SliceEx/fcnProfit.m
@@ -4,20 +4,12 @@
 % When prices are specified (explictly or implicitly), the profit is the total utility
 % of the slice minus the total payment to the slice provider;
 %%
-function [profit, grad] = fcnProfit(var_x, S, options)
+function [profit, grad] = fcnProfit(vars, slice, options)
 if nargout <= 1
-    if nargin == 2
-        profit = fcnProfit@Slice(var_x, S);
-    else
-        profit = fcnProfit@Slice(var_x, S, options);
-    end
-    profit = profit + S.constant_profit;
+    profit = fcnProfit@Slice(vars, slice, options);
+    profit = profit + slice.constant_profit;
 else
-    if nargin == 2
-        [profit, grad] = fcnProfit@Slice(var_x, S);
-    else
-        [profit, grad] = fcnProfit@Slice(var_x, S, options);
-    end
-    profit = profit - S.constant_profit;
+    [profit, grad] = fcnProfit@Slice(vars, slice, options);
+    profit = profit - slice.constant_profit;
 end
 end
