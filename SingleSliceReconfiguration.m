f5c694301473d6ef26f9b524af18dd1c56c8059c
Gang Wang <gary.wang.uestc@outlook.com>
2017-11-10 20:19:10 +0800

    Amend
    support adding static slice in the network.
    replace output storage class from struct to table.

--- SingleSliceReconfiguration.m
+++ SingleSliceReconfiguration.m
@@ -1,6 +1,6 @@
-global DEBUG g_results event_num; 
+global DEBUG g_results event_num InfoLevel; 
 event_num = 0;
-g_results = struct;
+g_results = table;
 DEBUG = true;
 
 %% TODO
@@ -8,8 +8,10 @@ DEBUG = true;
 
 %% Create Network
 if ~isfield(options, 'NetworkType')
-    warning('Network type is not provided, use the ''DynamicCloudAccessNetwork'' class.');
-    options.NetworkType = 'DynamicCloudAccessNetwork';
+    if InfoLevel.Global >= DisplayLevel.Notify
+        warning('Network type is not provided, use the ''DynamicCloudNetwork'' class.');
+    end
+    options.NetworkType = 'DynamicCloudNetwork';
 end
 PN = instantiateclass(options.NetworkType, node_opt, link_opt, VNF_opt, options);
 % otherwise
@@ -51,19 +53,35 @@ for t = 1:num_perm_type
     slice_opt.DuplicateFlow = true;
     seed_dynamic = seed_dynamic + 1;
     PSEB = SliceEntityBuilder(slice_opt);
-    for s = 1:1%type.PermanentCount(t)
+    for s = 1:type.PermanentCount(t)
         SFED.AddEntity(PSEB.Build(SFED.CurrentTime, []));
+    end
+end
+
+%% Add Static Slices
+if isfield(type, 'Static')
+    for t = 1:length(type.Static)
+        slice_opt = PN.slice_template(type.Static(t));
+        slice_opt.DuplicateFlow = true;
+        slice_opt.ClassName = type.StaticClass;         % specify the type of static slices.
+        for s = 1:type.StaticCount(t)
+            slice_opt.RandomSeed = seed_dynamic;        % |seed_dynamic| is provided in the configuration script.
+            seed_dynamic = seed_dynamic + 1;
+            % static slice is created and added, but resource allocation is defered until the first
+            % dynamic slice is added. 
+            PN.AddSlice(slice_opt);     
+        end
     end
 end
 
 %% Main loop
 iter_num = 0;
 while iter_num < NUM_EVENT
-    ev = SFED.nextEvent;
     %%%
     % *output*
     iter_num = iter_num + 1;
     total_iter_num = total_iter_num + 1;
     waitbar(total_iter_num/TOTAL_NUM, progress_bar, ...
             sprintf('Simulation Progress: %d/%d', total_iter_num, TOTAL_NUM));
+    ev = SFED.nextEvent;
 end
