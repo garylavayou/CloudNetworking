b48154ad32b01281e2e0d0c77b0d48af07336b23
Gary Wang <gary.wang.uestc@outlook.com>
2018-10-18 17:12:56 +0800

    Refactor and Reoprganization

---
+++ @SimpleDynamicSlice/executeMethod.m
@@ -0,0 +1,112 @@
+function [exitflag, fidx] = executeMethod(this, action)
+global g_results event_num DEBUG INFO;  %#ok<NUSED>
+this.temp_vars = struct();
+this.diff_state = struct([]);
+fidx = [];
+
+%%
+this.getAs_res;         % TODO, incremental upate of As_res, Hrep, and Hdiag.
+this.getHrep;
+this.getHdiag;
+% if this.options.ReconfigMethod >= ReconfigMethod.Dimconfig
+%     this.vnf_reconfig_cost = this.Parent.options.VNFReconfigCoefficient * ...
+%         repmat(this.VirtualDataCenters.ReconfigCost, this.NumberVNFs, 1);
+%     if this.ENABLE_DYNAMIC_NORMALIZER
+%         beta = this.getbeta();
+%         this.topts.vnf_reconfig_cost = beta.v*this.vnf_reconfig_cost;
+%     else
+%         this.topts.vnf_reconfig_cost = this.options.ReconfigScaler*this.vnf_reconfig_cost;
+%     end
+%     this.old_variables.v = this.VNFCapacity(:);
+% end
+tic
+t1 = tic;
+switch this.options.ReconfigMethod
+    case ReconfigMethod.Baseline
+        % provide 'method' and 'model' to customize the <optimalFlowRate>
+        % Since we adopt |FixedCost| model, the resource cost that is a
+        % constant, is not include in the objective value |profit|. So, we
+        % should exclude it from the |profit|, as well as the reconfiguration
+        % cost.
+        options.CostModel = 'fixcost';
+        options.SlicingMethod = SlicingMethod.AdjustPricing;
+        this.prices.Link = this.VirtualLinks.Price;
+        this.prices.Node = this.VirtualDataCenters.Price;
+        profit = this.optimalFlowRate(options);
+    case {ReconfigMethod.Fastconfig,ReconfigMethod.FastconfigReserve}
+        profit = this.fastReconfigure(action);
+    case {ReconfigMethod.Fastconfig2, ReconfigMethod.Fastconfig2Reserve}
+        profit = this.fastReconfigure2(action);
+    case {ReconfigMethod.Dimconfig, ReconfigMethod.DimconfigReserve, ...
+            ReconfigMethod.DimBaseline}
+        profit = this.DimensioningReconfigure(action);
+    otherwise
+        error('NetworkSlicing:UnsupportedMethod', ...
+            'error: unsupported method (%s) for network slicing.', ...
+            this.options.ReconfigMethod.char) ;
+end
+t2 = toc(t1);
+if this.options.ReconfigMethod>=ReconfigMethod.Dimconfig && isempty(profit)
+    exitflag = -1;
+    return;
+end
+% Check resource utilization
+%{
+idx = this.VirtualLinks.Capacity~=0;
+link_util = this.VirtualLinks.Load(idx)./this.VirtualLinks.Capacity(idx);
+sum_link_util = sum(this.VirtualLinks.Load(idx))./sum(this.VirtualLinks.Capacity(idx));
+mean_link_util = mean(link_util);
+%}
+
+% stat.Solution = this.Variables;
+stat = this.get_reconfig_stat();
+stat.Profit = profit;
+if ~isempty(DEBUG) && DEBUG 
+    disp(stat);
+    fprintf('%s elapsed time: %ds\n',this.options.ReconfigMethod.char, t2);
+end
+switch this.options.ReconfigMethod
+    case ReconfigMethod.Baseline
+        stat.ReconfigType = ReconfigType.Reconfigure;
+    case {ReconfigMethod.Fastconfig,ReconfigMethod.FastconfigReserve,...
+            ReconfigMethod.Fastconfig2, ReconfigMethod.Fastconfig2Reserve}
+        stat.ReconfigType = ReconfigType.FastReconfigure;
+    case {ReconfigMethod.Dimconfig, ReconfigMethod.DimconfigReserve}
+        stat.ReconfigType = ReconfigType.Dimensioning;
+        if ~this.b_dim
+            stat.ReconfigType = ReconfigType.FastReconfigure;
+        elseif this.options.Adhoc
+            % If the slice do not support Adhoc flows, then we do not release resource
+            % decriptors for the slice.
+            this.release_resource_description();
+        end
+    case ReconfigMethod.DimBaseline
+        if this.b_dim
+            if this.options.Adhoc'
+                this.release_resource_description();
+            end
+            stat.ReconfigType = ReconfigType.Dimensioning;
+        else
+            stat.ReconfigType = ReconfigType.Reconfigure;
+        end
+end
+if this.ENABLE_DYNAMIC_NORMALIZER
+    this.postl1normalizer();
+    if ~isempty(DEBUG) && DEBUG
+        disp(this.getbeta);
+    end
+end
+g_results(event_num,:) = stat;
+
+%% Reset temporary variables
+% These variables should be cleared. If it is used next time, it will be assigned with new
+% values. Some methods will execute according to the state (if it is initialized) of the
+% variables.
+this.prices.Link = [];
+this.prices.Node = [];
+this.topts = struct();
+this.changed_index = struct();
+this.net_changes = struct();
+this.b_dim = false;
+exitflag = 0;
+end
---
+++ @SimpleDynamicSlice/fastReconfigure2.m
@@ -0,0 +1,171 @@
+%% Fast Reconfiguration with VNF Instance Re-scaling
+% Comparing with <_fastReconfigure_>, this method also considers VNF instance re-scaling,
+% i.e., the capacity of VNF instances could change during reconfiguration.
+% See also <fastReconfigure>.
+function [profit,cost] = fastReconfigure2(this, action, new_opts)
+global DEBUG; %#ok<NUSED>
+
+if nargin <= 2
+    new_opts = struct;
+end
+options = structmerge(new_opts, ...
+    getstructfields(this.Parent.options, 'Form', 'default', 'normal'), ...
+    getstructfields(this.options, 'PricingPolicy', 'default', 'quadratic'), ...
+    getstructfields(this.options, 'ReconfigMethod'),...
+    'exclude');   
+
+if this.NumberFlows == 0
+    [profit, cost] = this.handle_zero_flow(options);
+    return;
+end
+
+NL = this.NumberLinks;
+NN = this.NumberServiceNodes;
+NP = this.NumberPaths;
+NV = this.NumberVNFs;
+
+%%% Formulate input for convex optimization (fmincon).
+% The problem has multiple inequalities, and the lowerbounds for variables.
+As_res = this.As_res;        % update As_res
+%% Save the VNF capacity to the previous state, berfore optimization
+% 'FastReconfig2' reconfigure VNF instance. After the slice is created, |VNFCapacity| is recorded.
+% After each optimization, the |VNFCapacity| is updated.
+this.old_variables.v = this.Variables.v;  
+this.update_reconfig_costv();              % update reconfigure cost with scaler.
+%%
+% List of constraints:
+%   (1) flow processing requirement: NP*NV (this.num_lcon_res);
+%   (2) VNF instance capacity constraint: NV*NN (this.num_varv); VNF load
+%       (y_nf) is no more than VNF instance capacity (v_nf);
+%   (3) Node capacity constraint: NN; VNF instance capacity is no more than
+%       node capacity. Since there is reconfiguration cost, we cannot use
+%       VNF load to express the constraint (Hrep*z). Instead we directly
+%       use VNF instance capacity as variables to express it
+%       (sum(v_nf)<=v_n). See also <Slice.getHrep>. 
+%   (4) Link capacity constraint: NL;
+%   (5) link reconfiguration cost constraint: 2*NP;
+%   (6) node reconfiguration cost constraint: 2*NN*NP*NV (2*this.num_varz);
+%   (5) VNF instance reconfiguration cost constraint: 2*NV*NN;
+num_lcon = this.num_lcon_res + this.num_varv + NN + NL + ...
+    2*NP + 2*this.num_varz + 2*this.num_varv;
+nnz_As = nnz(As_res) + (nnz(this.Hdiag)+this.num_varv) + nnz(this.Hrep) + nnz(this.I_edge_path) + ...
+    + 4*NP + 4*this.num_varz+ 4*this.num_varv;
+num_vars = 2*this.num_vars + 2*this.num_varv;
+As = spalloc(num_lcon, num_vars, nnz_As);
+As(1:this.num_lcon_res,1:this.num_vars) = As_res;
+row_offset = this.num_lcon_res;
+As(row_offset+(1:this.num_varv), NP+(1:this.num_varz)) = this.Hdiag;
+As(row_offset+(1:this.num_varv), this.num_vars+(1:this.num_varv)) = -eye(this.num_varv);
+row_offset = row_offset + this.num_varv;
+As(row_offset+(1:NN), this.num_vars+(1:this.num_varv)) = repmat(eye(NN),1,NV);
+row_offset = row_offset + NN;
+As(row_offset+(1:NL), 1:NP) = this.I_edge_path;
+row_offset = row_offset + NL;
+As(row_offset+(1:NP), 1:NP) = eye(NP);
+As(row_offset+(1:NP), num_vars/2+(1:NP)) = -eye(NP);
+row_offset = row_offset + NP;
+As(row_offset+(1:NP), 1:NP) = -eye(NP);
+As(row_offset+(1:NP), num_vars/2+(1:NP)) = -eye(NP);
+row_offset = row_offset + NP;
+As(row_offset+(1:this.num_varz), (NP+1):this.num_vars) = eye(this.num_varz);
+As(row_offset+(1:this.num_varz), (num_vars/2+NP)+(1:this.num_varz)) = -eye(this.num_varz);
+row_offset = row_offset + this.num_varz;
+As(row_offset+(1:this.num_varz), (NP+1):this.num_vars) = -eye(this.num_varz);
+As(row_offset+(1:this.num_varz), (num_vars/2+NP)+(1:this.num_varz)) = -eye(this.num_varz);
+row_offset = row_offset + this.num_varz;
+As(row_offset+(1:this.num_varv), (this.num_vars+1):num_vars/2) = eye(this.num_varv);
+As(row_offset+(1:this.num_varv), (num_vars/2+this.num_vars+1):end) = -eye(this.num_varv);
+row_offset = row_offset + this.num_varv;
+As(row_offset+(1:this.num_varv), (this.num_vars+1):num_vars/2) = -eye(this.num_varv);
+As(row_offset+(1:this.num_varv), (num_vars/2+this.num_vars+1):end) = -eye(this.num_varv);
+
+bs = [sparse(this.num_lcon_res+this.num_varv,1);
+    this.ServiceNodes.Capacity; % The field will only change in slice dimensionging.
+    this.Links.Capacity;
+    this.topts.old_variables_x;       % which have been pre-processed, so it can be
+    -this.topts.old_variables_x;      % compared with the current states.
+    this.topts.old_variables_z;
+    -this.topts.old_variables_z;
+    this.old_variables.v;      % Equal to last stage's VNF capacity, size not change
+    -this.old_variables.v];
+
+var0 = [this.topts.old_variables_x;
+    this.topts.old_variables_z;
+    this.old_variables.v;
+    %     this.topts.old_variables_x;
+    %     this.topts.old_variables_z;
+    %     this.old_variables.v
+    zeros(this.num_vars+this.num_varv,1)
+    ];
+assert(this.checkFeasible(var0), 'error: infeasible start point.');
+
+%% Perform optimization
+fmincon_opt = optimoptions(@fmincon);
+fmincon_opt.Algorithm = 'interior-point';
+fmincon_opt.SpecifyObjectiveGradient = true;
+fmincon_opt.Display = 'notify';
+if strcmpi(options.Form, 'compact')
+    %% get the compact formulation
+    % There are lots of zeros in $z_npf$, which could be determined by $h_np$.
+    % If a row is all zero, this row is in-active, and could be removed.
+    z_filter = sparse(repmat(...
+        reshape(logical(this.I_node_path), numel(this.I_node_path),1),...
+        NV,1));
+    this.I_active_variables = [true(NP,1);  z_filter;  true(this.num_varv,1);...
+        true(NP,1); z_filter; true(this.num_varv,1)];
+    row_offset = this.num_lcon_res + this.num_varv + NN + NL + 2*NP;
+    active_rows = [(1:row_offset)'; row_offset+find(z_filter); ...
+        row_offset+this.num_varz+find(z_filter); ...
+        ((num_lcon-2*this.num_varv+1):num_lcon)'];
+    As = As(active_rows, this.I_active_variables);
+    var0 = var0(this.I_active_variables);
+    bs = bs(active_rows);
+    fcn_opts.num_varz = nnz(z_filter);
+    this.topts.z_reconfig_cost = this.topts.z_reconfig_cost(z_filter);
+    fcn_opts.bCompact = true;
+else
+    fcn_opts.num_varz = this.num_varz;
+end
+lbs = sparse(length(var0),1);
+fcn_opts.num_varx = this.NumberPaths;
+fcn_opts.num_varv = this.num_varv;
+fmincon_opt.HessianFcn = ...
+    @(x,lambda)DynamicSlice.hessReconfigure(x, lambda, this, fcn_opts);
+[xs, fval, exitflag, output] = ...
+    fmincon(@(x)DynamicSlice.fcnFastConfigProfit(x, this, fcn_opts), ...
+    var0, As, bs, [], [], lbs, [], [], fmincon_opt);
+if strcmpi(options.Form, 'compact')
+    x = zeros(num_vars, 1);
+    x(this.I_active_variables) = xs;
+else
+    x = xs;
+end
+
+%% Reconfiguration Cost in Problem Formulation
+% comparing the old variables with new variables to decide the reconfiguration
+% cost.
+% Since the two vector has different number of elements, we should comparing
+% it accordingly, and set aside the variables of new arriving/departing flow.
+
+% x is a local solution to the problem when exitflag is positive.
+this.interpretExitflag(exitflag, output.message);
+options.Action = action;    % This might be used when check feasible solution.
+options.ConstraintTolerance = fmincon_opt.ConstraintTolerance;
+assert(this.checkFeasible(x,options), 'error: infeasible solution.');
+
+%%%
+% The post processing like <optimalFlowRate> is not needed, since the
+% objective function (reconfiguration cost) will force those variables to be zero.
+this.convert(x, 0);
+this.flow_rate = this.getFlowRate(this.temp_vars.x);
+this.postProcessing();
+this.setPathBandwidth;
+this.FlowTable.Rate = this.getFlowRate;
+this.Links.Load = this.getLinkLoad;
+this.ServiceNodes.Load = this.getNodeLoad;
+if nargout >= 1
+    cost = this.getSliceCost(options.PricingPolicy, 'const');
+    rc_linear = this.get_reconfig_cost('linear', true);
+    profit = -fval - cost + rc_linear;
+end
+end
